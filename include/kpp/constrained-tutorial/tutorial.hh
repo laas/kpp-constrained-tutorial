4b9fcbedd374561036288911c382f74a6fb8c378
Sébastien Dalibard <sebastien.dalibard@gmail.com>
2011-11-21 12:54:11 +0100

    First working version of package

---
+++ include/kpp/constrained-tutorial/tutorial.hh
@@ -0,0 +1,65 @@
+// Copyright (C) 2011 by Sebastien Dalibard.
+//
+// This file is part of the kpp-constrained-tutorial.
+//
+// kpp-constrained-tutorial is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// kpp-constrained-tutorial is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with kpp-constrained-tutorial.  If not, see <http://www.gnu.org/licenses/>.
+
+
+#ifndef KPP_CONSTRAINED_TUTORIAL_KPP_CONSTRAINED_TUTORIAL_HH_
+#define KPP_CONSTRAINED_TUTORIAL_KPP_CONSTRAINED_TUTORIAL_HH_
+
+#include <string>
+
+#include <KineoUtility/kitDefine.h>
+
+#include <kppInterface/kppInterface.h>
+
+#include <hpp/constrained/planner/grasping-planner.hh>
+
+namespace kpp {
+  namespace constrained {
+    KIT_PREDEF_CLASS(Tutorial);
+    class Tutorial : public CkppInterface
+    {
+    public:
+      
+      typedef hpp::constrained::GraspingPlanner Planner;
+
+      static TutorialShPtr create();
+
+      virtual ~Tutorial();
+
+      virtual std::string name() const;
+      
+    protected:
+
+      Tutorial(Planner * i_hppPlanner);
+
+      ktStatus init(const TutorialWkPtr & i_wkPtr);
+
+      Planner * hppPlanner_;
+
+    private:
+      
+      TutorialWkPtr wkPtr_;
+
+  };
+
+  } // end of namespace constrained-tutorial
+} // end of namespace kpp
+
+extern "C" KPP_ADDON_API int
+initializeModule (CkppModuleInterfaceShPtr& o_moduleInterface);
+
+#endif //! KPP_CONSTRAINED_TUTORIAL_KPP_CONSTRAINED_TUTORIAL_HH_
---
+++ src/tutorial.cc
@@ -0,0 +1,81 @@
+// Copyright (C) 2011 by Sebastien Dalibard.
+//
+// This file is part of the kpp-constrained-tutorial.
+//
+// kpp-constrained-tutorial is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// kpp-constrained-tutorial is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with kpp-constrained-tutorial.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <kpp/constrained-tutorial/tutorial.hh>
+
+namespace kpp {
+  namespace constrained {
+
+    Tutorial::Tutorial(Planner * i_planner):
+      CkppInterface(i_planner),
+      hppPlanner_(i_planner)
+    {
+    }
+
+    Tutorial::~Tutorial()
+    {
+    }
+
+    TutorialShPtr
+    Tutorial::create()
+    {
+      Planner * planner = new Planner();
+
+      TutorialShPtr shPtr(new Tutorial(planner));
+      TutorialWkPtr wkPtr(shPtr);
+
+      if (shPtr->init (wkPtr) != KD_OK)
+	shPtr.reset ();
+      return shPtr;
+    }
+
+    ktStatus
+    Tutorial::init(const TutorialWkPtr & i_wkPtr)
+    {
+     if (CkppInterface::init (i_wkPtr) != KD_OK)
+	return KD_ERROR;
+      wkPtr_ = i_wkPtr;
+      return KD_OK;
+    }
+
+    std::string
+    Tutorial::name() const
+    {
+      return std::string("kpp-constrained-tutorial");
+    }
+
+  } // end of namespace constrained-tutorial
+} // end of namespace kpp
+
+
+extern "C"
+int
+initializeModule (CkppModuleInterfaceShPtr& o_moduleInterface)
+{
+  using namespace kpp::constrained;
+ 
+  o_moduleInterface = Tutorial::create ();
+
+  if (o_moduleInterface)
+    return 0;
+
+  //FIXME: for some reason, a segv happens if the module initialization
+  // fails. As the user is doomed anyway, let's exit with a nice error
+  // message.
+
+  return 1;
+}
